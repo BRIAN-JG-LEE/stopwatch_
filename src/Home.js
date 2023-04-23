184a7a0c49c30175c86a74c391de9544f1a713af
BRIAN-JG-LEE <103700824+BRIAN-JG-LEE@users.noreply.github.com>
2023-04-24 06:59:21 +0900

    Add files via upload

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,15 @@ const Home = () => {
             <Helmet>
                 <title>Free Stopwatch</title>
                 <meta name="description" content="Free stopwatch for everyone. This website is made Create-react-app. You can experience precise, accurate stopwatch easy to use" data-react-helmet="true"/>
+
+                <link rel="manifest" href="public/manifest.json" />
+                <link rel="apple-touch-icon" sizes="180x180" href="public/apple-touch-icon.png"/>
+                <link rel="icon" type="image/png" sizes="16x16" href="public/favicon-16x16.png"/>
+                <link rel="manifest" href="public/site.webmanifest"/>
+                <link rel="mask-icon" href="public/safari-pinned-tab.svg" color="#5bbad5"/>
+                <meta name="msapplication-TileColor" content="#da532c"/>
+                <meta name="theme-color" content="#ffffff"/>
+                <link rel="icon" type="image/png" sizes="32x32" href="public/favicon-32x32.png"/>
             </Helmet>
         </div>
     );
