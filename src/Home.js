940046c5d6ced3b5abfd606190c7765700f921f9
BRIAN-JG-LEE <103700824+BRIAN-JG-LEE@users.noreply.github.com>
2023-04-24 06:54:36 +0900

    Add files via upload

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+const Home = () => {
+    return (
+        <div>
+            <Helmet>
+                <title>Free Stopwatch</title>
+                <meta name="description" content="Free stopwatch for everyone. This website is made Create-react-app. You can experience precise, accurate stopwatch easy to use" data-react-helmet="true"/>
+            </Helmet>
+        </div>
+    );
+};
+
+export default Home;
