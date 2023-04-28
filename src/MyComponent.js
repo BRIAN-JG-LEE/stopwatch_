08685404359c003b890ddeec24ffe8fc5335fc61
BRIAN-JG-LEE <103700824+BRIAN-JG-LEE@users.noreply.github.com>
2023-04-28 13:00:42 +0900

    Add files via upload

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -24,6 +24,8 @@ function MyComponent() {
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
       <link rel="manifest" href="/site.webmanifest"/>
       <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
+      <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+
       <meta name="msapplication-TileColor" content="#da532c"/>
       <meta name="theme-color" content="#ffffff"/>
       <script defer="defer" src="/static/js/main.95488d6e.js"></script>
